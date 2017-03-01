02b340ba3729eec27341d54f4afbeca73ca11ad8
ambud <asharma525xi@gmail.com>
2017-03-01 12:03:33 -0600

    Fixing unit test

--- core/src/test/java/com/srotya/sidewinder/core/aggregators/TestCalculusAggregators.java
+++ core/src/test/java/com/srotya/sidewinder/core/aggregators/TestCalculusAggregators.java
@@ -42,11 +42,11 @@ public class TestCalculusAggregators {
 			dps.add(new DataPoint(ts, d));
 		}
 		ReducingWindowedAggregator rwa = new DerivativeFunction();
-		rwa.init(new Object[] { 70_000, "mean" });
+		rwa.init(new Object[] { 70, "smean" });
 		List<DataPoint> result = rwa.aggregate(dps);
 		// 1.65 and 3.85
 		assertEquals(1, result.size());
-		assertEquals(0.00003666666667 * 1000, result.get(0).getValue() * 1000, 0.01);
+		assertEquals(0.00003666666667 * 1000, result.get(0).getValue(), 0.01);
 		System.out.println(result.get(0).getValue() * 1000 + "\t" + ts);
 	}
 
@@ -61,7 +61,7 @@ public class TestCalculusAggregators {
 			dps.add(new DataPoint(ts, d));
 		}
 		ReducingWindowedAggregator rwa = new DerivativeFunction();
-		rwa.init(new Object[] { 20_000, "mean" });
+		rwa.init(new Object[] { 20, "smean" });
 		List<DataPoint> result = rwa.aggregate(dps);
 		assertEquals(1, result.size());
 		assertEquals(false, result.get(0).isFp());
